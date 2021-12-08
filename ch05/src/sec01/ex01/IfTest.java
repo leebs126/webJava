ec7b5f5c39aec3f35c6b8b10ed222b72f8d99b2f
user <user@121.169.58.64>
2021-12-08 16:03:47 +0900

    5장 if문 반영

---
+++ ch05/src/sec01/ex01/IfTest.java
@@ -0,0 +1,19 @@
+package sec01.ex01;
+
+public class IfTest {
+
+	public static void main(String[] args) {
+		int score = 65;
+
+		if (score >= 60) {
+			System.out.println("60점보다 큽니다.");
+			System.out.println("합격입니다.");
+		}
+
+		if (score < 60)
+			System.out.println("60점 미만입니다.");
+			System.out.println("불합격입니다.");
+
+	}
+
+}
---
+++ ch05a/src/sec01/ex01/IfTest.java
@@ -0,0 +1,28 @@
+package sec01.ex01;
+
+public class IfTest {
+
+	public static void main(String[] args) {
+		int score = 65;
+		
+		//if(score = 65) {  
+		if(score == 65) {
+			System.out.println("65점입니다.");
+		}
+
+		if (score >= 60) {
+			System.out.println("60점 이상입니다.");
+			System.out.println("합격입니다.");
+		}
+		
+		if(score < 60) 
+			System.out.println("60점 미만입니다.");
+			System.out.println("불합격입니다");
+			
+		
+//		if(score == 65) {
+//			System.out.println("65점입니다.");
+//		}
+	}
+
+}
