c3a57aca71b05cf58dbb9bddff716e2d78a906fb
user <user@DESKTOP-7RFE112>
2022-01-29 19:03:58 +0900

    48강 내용 반영

---
+++ ch06a/src/sec04/ex01/MatrixAddTest.java
@@ -0,0 +1,38 @@
+package sec04.ex01;
+
+public class MatrixAddTest {
+
+	public static void main(String[] args) {
+		int[][] matA = {{2,  3,  5},
+						{6,  7,  8},
+						{9, 10, 11}};
+		
+		int[][] matB = {{ 1,  4,  7},
+						{13,  3,  2},
+						{ 3,  9, 20}};
+		
+		int[][] matC = new int[matA.length][matA[0].length];
+		
+		//두 행렬의 합을 구합니다.
+		for(int i = 0; i< matA.length; i++) {
+			for(int j= 0; j <matA[i].length;j++) {
+				matC[i][j] = matA[i][j] + matB[i][j];
+			}
+		}
+		
+		
+		System.out.println("두 행렬의 합");
+		System.out.println("--------------");
+		for(int i = 0; i< matA.length; i++) {
+			for(int j= 0; j <matA[i].length;j++) {
+				System.out.print(matC[i][j]+ "\t");
+			}
+			System.out.println();
+		}
+		
+		
+
+
+	}
+
+}
