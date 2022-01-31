d17619a0d9f0b8416d40040b02cf7cf5c209d411
user <user@DESKTOP-7RFE112>
2022-01-31 14:13:28 +0900

    50강 내용 반영

--- ch06/src/sec04/ex02/TwoDimArrayValueSettingTest_a.java
+++ ch06/src/sec04/ex02/TwoDimArrayValueSettingTest_a.java
@@ -1,30 +1,36 @@
 package sec04.ex02;
 
-public class TwoDimArrayValueSettingTest2 {
+public class TwoDimArrayValueSettingTest_a {
 
 	public static void main(String[] args) {
 		int[][] arr = new int[4][4];
 
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = 1;
-			}
-		}
-		
-		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				if(i + j < 4) {
+				if (i == 0) {
 					arr[i][j] = 2;
-				}else {
-					arr[i][j] = 1;
+					
+				} else if (i == 1) {
+					if (j != 3)
+						arr[i][j] = 2;
+					else
+						arr[i][j] = 1;
+					
+				} else if (i == 2) {
+					if (j == 0 || j == 1)
+						arr[i][j] = 2;
+					else
+						arr[i][j] = 1;
+					
+				} else if (i == 3) {
+					if (j == 0)
+						arr[i][j] = 2;
+					else
+						arr[i][j] = 1;
 				}
-				
 			}
 		}
-		
-		System.out.println("2차원 배열 값");
-		System.out.println("-------------------------");
+
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + "\t");
--- ch06a/src/sec04/ex02/TwoDimArrayValueSettingTest2.java
+++ ch06a/src/sec04/ex02/TwoDimArrayValueSettingTest2.java
@@ -3,18 +3,19 @@ package sec04.ex02;
 public class TwoDimArrayValueSettingTest2 {
 
 	public static void main(String[] args) {
-		int[][] arr = new int[4][4];
+		int[][] arr = new int[10][10];
 
+		// 1로 요소들을 초기화합니다.
+//		for (int i = 0; i < arr.length; i++) {
+//			for (int j = 0; j < arr[i].length; j++) {
+//				arr[i][j] = 1;
+//			}
+//		}
+
+		// 값을 2로 세팅합니다.
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = 1;
-			}
-		}
-		
-		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
-				if(i + j < 4) {
+				if(i + j < 10) {
 					arr[i][j] = 2;
 				}else {
 					arr[i][j] = 1;
@@ -22,8 +23,9 @@ public class TwoDimArrayValueSettingTest2 {
 				
 			}
 		}
-		
-		System.out.println("2차원 배열 값");
+
+		// 배열 값들을 출력합니다.
+		System.out.println("배열 값 출력");
 		System.out.println("-------------------------");
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
