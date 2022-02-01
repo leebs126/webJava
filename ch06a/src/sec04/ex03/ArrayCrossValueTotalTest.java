54b2a0fffeae89e20c9d36d7826dcd3e7607dce9
user <user@DESKTOP-7RFE112>
2022-02-01 18:26:23 +0900

    51강 내용 반영

---
+++ ch06a/src/sec04/ex03/ArrayCrossValueTotalTest.java
@@ -0,0 +1,27 @@
+package sec04.ex03;
+
+public class ArrayCrossValueTotalTest {
+
+	public static void main(String[] args) {
+		int[][] arr = {{2,  8, 9, 13, 13},
+				       {1,  7, 6, 12,  2},
+				       {1, 11, 9,  2, 20},
+				       {14, 3, 1, 22,  3},
+				       {14,16, 5,  3, 33}};
+		
+		int crossTotal = 0;
+		
+		for(int i=0; i<arr.length;i++) {
+			for(int j=0; j < arr[i].length;j++) {
+				
+				if(i == j || i + j == 4) {
+					crossTotal += arr[i][j];
+				}
+			}
+		}
+		
+		System.out.println("두 대각선 요소들의 합 : " + crossTotal);
+
+	}
+
+}
