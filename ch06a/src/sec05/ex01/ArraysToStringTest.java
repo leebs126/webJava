c6037dcb1008255b4907c1e1ca3388da1c925306
user <user@121.169.58.64>
2022-02-03 16:53:26 +0900

    52강 원고 반영

---
+++ ch06a/src/sec05/ex01/ArraysToStringTest.java
@@ -0,0 +1,19 @@
+package sec05.ex01;
+
+import java.util.Arrays;
+
+public class ArraysToStringTest {
+
+	public static void main(String[] args) {
+		int[] arr1 = {10, 20, 30, 40,50};
+		
+		for(int i=0; i<arr1.length;i++) {
+			System.out.print(arr1[i] +", ");
+		}
+		
+//		System.out.println();
+//		System.out.println(Arrays.toString(arr1));
+
+	}
+
+}
