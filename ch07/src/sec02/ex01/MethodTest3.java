2076e196232fa2b4cad267b4df5ac119e41b9615
user <user@121.169.58.64>
2022-02-27 18:43:50 +0900

    71강 원고 내용 반영

---
+++ ch07/src/sec02/ex01/MethodTest3.java
@@ -0,0 +1,34 @@
+package sec02.ex01;
+
+import java.util.Scanner;
+
+public class MethodTest3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수를 입력해 주세요 : ");
+		int num = sc.nextInt();
+		
+		MethodTest3 m = new MethodTest3();
+		int result = m.digitSum(num);
+		System.out.println(num + "의 자리수의 합 : " + result);
+
+	}
+
+	public int digitSum(int val) {
+		// 1. 지역변수 선언
+		int total = 0;
+
+		// 2. 기능 구현
+		while (true) {
+			total += val % 10;
+			val /= 10;
+			if (val == 0)
+				break;
+		}
+
+		//3.결과값 출력또는 반환
+		return total;
+	}
+
+}
