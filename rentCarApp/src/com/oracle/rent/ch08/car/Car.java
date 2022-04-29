c584927f5643b3094c2b9bed6c60a2bc5bbbcf82
user <user@61.79.197.179>
2022-04-29 18:19:08 +0900

    25강(8-25) 원고 내용 반영

---
+++ rentCarApp/src/com/oracle/rent/ch08/car/Car.java
@@ -0,0 +1,55 @@
+package com.oracle.rent.ch08.car;
+
+public class Car {
+	String carNumber;
+	String carName;
+	String carColor;
+	int carSize;
+	String carMaker;
+
+	public Car() {
+		
+	}
+	
+	public Car(String _carNumber, String _carName, String _carColor, int _carSize, String _carMaker) {
+		carNumber = _carNumber;
+		carName = _carName;
+		carColor = _carColor;
+		carSize = _carSize;
+		carMaker = _carMaker;
+	}
+	
+	
+	// 차 정보 조회
+	public String checkCarInfo() {
+		System.out.println("\n렌트카 정보를 조회합니다.");
+		System.out.println("-------------------------");
+		String carInfo = "차번호 : " + carNumber 
+				           +"\n차이름 : " + carName
+				           +"\n차색상: " + carColor 
+				           +"\n차크기 : " + carSize +"cc"
+				           +"\n차제조사: " + carMaker;
+		return carInfo;
+	}
+
+	//차 정보 등록
+	public void regCarInfo(String _carNumber, String _carName, String _carColor, int _carSize, String _carMaker) {
+		System.out.println("차 정보를 등록합니다.");
+		carNumber = _carNumber;
+		carName = _carName;
+		carColor = _carColor;
+		carSize = _carSize;
+		carMaker = _carMaker;
+	}
+
+	//차 정보 수정
+	public void modCarInfo() {
+		System.out.println("\n렌트카 정보를 수정합니다.");
+	}
+
+	//차 정보 삭제
+	public void delCarInfo() {
+		System.out.println("\n렌트카 정보를 삭제합니다.");
+	}
+
+}
---
+++ rentCarApp/src/com/oracle/rent/ch08/res/Reserve.java
@@ -0,0 +1,50 @@
+package com.oracle.rent.ch08.res;
+
+public class Reserve {
+	String resCarNumber;
+	String resDate;
+	String useBeginDate;
+	String useEndDate;
+	
+	public Reserve() {
+		
+	}
+	
+	public Reserve(String _resCarNumber, String _resDate, String _useBeginDate, String _useEndDate) {
+		resCarNumber = _resCarNumber;
+		resDate = _resDate;
+		useBeginDate = _useBeginDate;
+		useEndDate = _useEndDate;
+	}
+
+	// 차 예약 정보 조회 기능 
+	public String  checkResInfo() {
+		System.out.println("\n차 예약 정보를 조회합니다.");
+		System.out.println("-------------------------");
+		String resInfo = "예약차 번호 : " + resCarNumber
+				        +"\n예약일자 : " + resDate
+				        +"\n차사용시작일자 : " + useBeginDate
+				        +"\n차반납예정일자 : " + useEndDate; 
+		return resInfo;
+	}
+	
+	// 차 예약 기능
+	public void resCar(String _resCarNumber, String _resDate, String _useBeginDate, String _useEndDate) {
+		System.out.println("렌터카를 예약합니다.");
+		resCarNumber = _resCarNumber;
+		resDate = _resDate;
+		useBeginDate = _useBeginDate;
+		useEndDate = _useEndDate;
+	}
+
+	// 차 예약정보 수정 기능
+	public void modResInfo() {
+		System.out.println("\n차 예약정보를 수정합니다..");
+	}
+
+	// 차 예약 정보 취소 기능
+	public void cancelResInfo() {
+		System.out.println("\n차 예약을 취소합니다.");
+	}
+
+}
