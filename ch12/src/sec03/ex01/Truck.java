de681c67086b5c6267d81eeb67330252ad0daa73
leebs <leebs@118.32.103.181>
2023-11-08 11:53:56 +0900

    소스 코드 정리

--- ch12/src/sec03/ex01/Truck.java
+++ ch12/src/sec03/ex01/Truck.java
@@ -1,6 +1,7 @@
 package sec03.ex01;
 
-public class Truck  extends Car{
+//public class Truck  extends Car{
+public class Truck {
 	private int load;
 	public Truck() {
 		System.out.println("Truck 생성자 호출");
--- ch12/src/sec03/ex02/Truck.java
+++ ch12/src/sec03/ex02/Truck.java
@@ -1,9 +1,10 @@
 package sec03.ex02;
 
 public class Truck extends Car {
-	@Override
-	public void speedUp() {
-		velocity += 30;
-	}
+	//final로 지정된 메서드는 오버라이딩이 금지됩니다.
+//	@Override
+//	public void speedUp() {
+//		velocity += 30;
+//	}
 
 }
