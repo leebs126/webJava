0dc5adbc54c60ea4935068905c376ce03866a9be
leebs <leebs@118.32.103.181>
2023-11-08 11:51:22 +0900

    수정반영

--- ch03/src/sec01/ex01/VarInitTest.java
+++ ch03/src/sec01/ex01/VarInitTest.java
@@ -5,7 +5,7 @@ public class VarInitTest {
 	public static void main(String[] args) {
 		int num;
 //		System.out.println(num);  //초기화 하지 않고 변수 사용 시 에러가 발생합니다.
-		
+				
 		num = 12;
 //		int num = 12;
 		System.out.println("num = " + num);
