fd6c52a6e5625d334a3166982a9383a7cb19bbe5
leebs <leebs@220.72.89.15>
2023-12-30 13:31:56 +0900

    변수명 변경

--- ch09/src/sec03/ex05/EmpTest.java
+++ ch09/src/sec03/ex05/EmpTest.java
@@ -5,18 +5,18 @@ public class EmpTest {
 	public static void main(String[] args) {
 		String name;
 		int totalPay;
-		Employee Hong, Lee;
-		Lee = new Regular("이순신", 2000000, 5000000);
-		Hong = new Temp("홍길동", 1500000, 0, 500);
+		Employee hong, lee;
+		lee = new Regular("이순신", 2000000, 5000000);
+		hong = new Temp("홍길동", 1500000, 0, 500);
 		
 		System.out.println("사원 정보 출력");
 		System.out.println("-----------------------------------------------");
-		name = Lee.name;
-		totalPay = Lee.calcTotalPay();
+		name = lee.name;
+		totalPay = lee.calcTotalPay();
 		System.out.println("사원 이름: " + name + ", 총급여: " + totalPay + "원");
 		
-		name = Hong.name;
-		totalPay = Hong.calcTotalPay(); // 오버라이딩 된 메서드 호출
+		name = hong.name;
+		totalPay = hong.calcTotalPay(); // 오버라이딩 된 메서드 호출
 		System.out.println("사원 이름: " + name + ", 총급여: " + totalPay + "원");
 
 	}
