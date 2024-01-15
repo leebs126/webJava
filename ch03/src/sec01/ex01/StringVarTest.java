1e277435b2023fb1b35e33121c1c77d6f7035d86
leebs <leebs@220.72.89.27>
2024-01-15 13:15:16 +0900

    3장 소스코드 수정 및 보강

---
+++ ch03/src/sec01/ex01/StringVarTest.java
@@ -0,0 +1,14 @@
+package sec01.ex01;
+
+public class StringVarTest {
+	public static void main(String[] args) {
+		String name;
+		name = "홍길동";
+//		String name = "홍길동";
+		
+		System.out.println("이름은 " + name);
+		name = "이순신";
+		System.out.println("이름은 " + name);
+	}
+
+}
