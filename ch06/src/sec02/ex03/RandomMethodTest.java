5305778ad015fdb501c52d3c264d915ec7e25a3d
leebs <leebs@121.129.213.204>
2024-03-02 12:00:10 +0900

    배열 난수 예제 수저

--- ch06/src/sec02/ex03/RandomMethodTest.java
+++ ch06/src/sec02/ex03/RandomMethodTest.java
@@ -3,17 +3,12 @@ package sec02.ex03;
 public class RandomMethodTest {
 
 	public static void main(String[] args) {
-//		System.out.println(Math.random());  //0에서 1사이의 실수를 리턴합니다.
+//		System.out.println(Math.random());
 		
-		int rand = (int)(Math.random() * 100* 9) %  10 + 1;  //1 ~ 10
-		System.out.println("양의 정수 : " + rand);
-		
+		int rand = (int)(Math.random() * 100) + 1;  //1-100사이의 정수
+		System.out.println("난수: " + rand);
 
-		rand = (int)(Math.random() * 1000 * 200) % (200 - 20) + 20;  //20 - 200
-		System.out.println("양의 정수 : " + rand);
-		
-		
-
+		int rand2 = (int)(Math.random() * (200 - 100 +1)) + 100;  //20 - 200 사이 정수
+		System.out.println("난수: " + rand2);
 	}
-
 }
