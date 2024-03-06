70503539f1bd3a462cf1e0c09fc4bc8751bf90ff
leebs <leebs@121.129.213.204>
2024-03-06 15:19:51 +0900

    2차원 배열 예제 수정

--- ch06/src/sec04/ex03/ArrayCrossValueTotalTest.java
+++ ch06/src/sec04/ex03/ArrayCrossValueTotalTest.java
@@ -13,7 +13,7 @@ public class ArrayCrossValueTotalTest {
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
-				if (i == j || i + j == 4 ) {
+				if (i == j || i + j == arr.length -1 ) {
 					crossTotal += arr[i][j];
 				}
 			}
