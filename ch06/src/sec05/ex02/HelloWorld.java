9d1c07eae32ff598b7d44e43a97745dabf77bbad
leebs <leebs@121.129.213.204>
2024-03-08 12:11:54 +0900

    println 수정

--- ch06/src/sec05/ex02/HelloWorld.java
+++ ch06/src/sec05/ex02/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		if (args.length != 0) {
 			for (int i = 0; i < args.length; i++) {
-				System.out.print(args[i] + " ");
+				System.out.println(args[i] + " ");
 			}
 		}
 		
