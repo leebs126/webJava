1819e75d1560c3c9959e4551805ab2ececeed93c
leebs <leebs@121.129.213.204>
2024-04-02 19:46:33 +0900

    Car  클래스 메서드 수정

--- ch08/src/sec06/ex01/Car.java
+++ ch08/src/sec06/ex01/Car.java
@@ -1,21 +1,20 @@
 package sec06.ex01;
 
 public class Car {
-	//필드
+	// 필드
 	String carNumber;
 	String carName;
 	String carColor;
 	int carSize;
 	int velocity;
-	
-	//생성자
+
+	// 생성자
 	public Car() {
 		System.out.println("기본 Car 클래스 생성자 호출");
-		
+
 	}
-	
-	
-	public Car(String _carNumber, String _carName, String _carColor, int _carSize, int _velocity ){
+
+	public Car(String _carNumber, String _carName, String _carColor, int _carSize, int _velocity) {
 		System.out.println("매개변수가 있는 Car 클래스 생성자 호출");
 		carNumber = _carNumber;
 		carName = _carName;
@@ -23,44 +22,42 @@ public class Car {
 		carSize = _carSize;
 		velocity = _velocity;
 	}
-	
+
+	// 주행 기능 
+	public void drive() {
+		System.out.println("차를 운행합니다.");
+	}
+
+	// 속도를 증가하는 기능
 	public void speedUp() {
 		velocity = velocity + 1;
 	}
-	
-	
+
+	// 속도를 감소하는 기능
 	public void speedDown() {
 		velocity = velocity - 1;
-		if(velocity < 0) {
+		if (velocity < 0) {
 			velocity = 0;
 		}
 	}
-	
+
+	// 정지 기능 메서드
 	public void stop() {
 		System.out.println("브레이크를 밟아서 정지합니다.");
 		velocity = 0;
 	}
-	
+
+	// 주차 기능 메서드
 	public void park() {
 		System.out.println("주차합니다.");
 		velocity = 0;
 	}
-	
-	
-	
+
+	// 차량 정보를 출력하는 메서드
 	public void getCarInfo() {
-		System.out.println("차번호 : " + carNumber +
-				            "\n차이름 : " + carName +
-				            "\n차색상 : " +carColor + 
-				            "\n배기량 : " + carSize+ "cc" +  
-				            "\n현재 속도 : " + velocity + "km/h");
-		
+		System.out.println("차번호 : " + carNumber + "\n차이름 : " + carName + "\n차색상 : " + carColor + "\n배기량 : " + carSize
+				+ "cc" + "\n현재 속도 : " + velocity + "km/h");
+
 	}
-	
-	
-	
-	
-	
-	
 
 }
