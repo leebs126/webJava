ca338d7ae60eb09e1018f952c52201b37cefa9e4
leebs <leebs@59.5.237.13>
2024-04-16 16:34:02 +0900

    생성자 메시지 수정

--- ch09/src/sec02/ex01/College.java
+++ ch09/src/sec02/ex01/College.java
@@ -4,7 +4,7 @@ public class College extends Student {
 	private int courses; //학점
 	
 	public College(String _name, int _grade, int _courses) {
-		System.out.println("College 클래스의 기본 생성자 호출");
+		System.out.println("College 클래스의 생성자 호출");
 		name = _name;
 		grade = _grade;
 		courses = _courses;
