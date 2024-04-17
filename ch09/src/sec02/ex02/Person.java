17032893dd8e4617c1824afdf5a1f212988ecaed
leebs <leebs@59.5.237.13>
2024-04-17 12:02:19 +0900

    생성자 메시지 수정

--- ch09/src/sec02/ex02/Person.java
+++ ch09/src/sec02/ex02/Person.java
@@ -9,7 +9,7 @@ public class Person {
 	
 	}
 	public Person(String _gender, int _age) {
-		System.out.println("Person 기본 생성자 호출");
+		System.out.println("Person 매개변수가 2개인 생성자 호출");
 		gender = _gender;
 		age = _age;
 	}
