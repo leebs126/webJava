c6874ce080ccd107934bf0df5731687a8ec5e3dc
leebs <leebs@59.5.237.13>
2024-04-17 19:40:40 +0900

    super 기능 수정

--- ch09/src/sec02/ex03/College.java
+++ ch09/src/sec02/ex03/College.java
@@ -4,17 +4,17 @@ public class College extends Student {
 	private int courses; 
 	
 	public College() {
-		this("이순신", 2, 20);
 		System.out.println("College 클래스의 기본 생성자 호출");
 		
 	}
-	public College(String name, int grade, int courses) {
+	public College(String name, int grade, int _courses) {
 		super();
+		//super(name, grade); //매개변수 있는 Student 생성자를 명시적으로 호출합니다.
 		System.out.println("매개변수가 3개인 College 생성자 호출");
 		
 		super.name = name;
 		super.grade = grade;
-		this.courses = courses;
+		courses = _courses;
 	}
 	
 	public int getCourses() {
