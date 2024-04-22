cc5b8508e37e50ddb76b124bf193dc82338a822d
leebs <leebs@59.5.237.13>
2024-04-22 16:02:11 +0900

    버스 클래스 생성자 수정

--- ch09/src/sec05/ex01/Bus.java
+++ ch09/src/sec05/ex01/Bus.java
@@ -9,6 +9,7 @@ public class Bus extends Car {
 		System.out.println("Bus 클래스 생성자 호출");
 		super.carName = carName;
 		super.carNumber = carNumber;
+		super.carColor = carColor;
 		super.velocity = velocity;
 		super.carSize = carSize;
 		this.passenger = passenger;
