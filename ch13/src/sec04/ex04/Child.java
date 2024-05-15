e5dcd9cfa5b2a0272892e540ef2687036714b889
leebs <leebs@59.5.237.13>
2024-05-15 17:32:32 +0900

    소스코드 수정

--- ch13/src/sec04/ex04/Child.java
+++ ch13/src/sec04/ex04/Child.java
@@ -4,17 +4,17 @@ public class Child implements ITest3 {
 
 	@Override
 	public void method1() {
-		System.out.println("method1() 호출");
+		System.out.println("상속 받는 ITest1의 method1() 호출");
 	}
 	
 	@Override
 	public void method2() {
-		System.out.println("method2() 호출");
+		System.out.println("상속 받는 ITest2의 method2() 호출");
 	}
 
 	@Override
 	public void method3() {
-		System.out.println("상속 받는 ITest3의 method3() 호출");
+		System.out.println("method3() 호출");
 		
 	}
 
--- ch13/src/sec04/ex04/ChildTest.java
+++ ch13/src/sec04/ex04/ChildTest.java
@@ -13,6 +13,7 @@ public class ChildTest {
 		i1.method1();  
 //		i1.method2();  //인터페이스에 선언되지 않은 메소드는 호출 불가능합니다.
 //		i1.method3();
+		System.out.println();
 		
 		ITest3 i3 = new Child();
 		i3.method1();  //상속받으므로 상위 메소드를 모두 호출할 수있습니다.
