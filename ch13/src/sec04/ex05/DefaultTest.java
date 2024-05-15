21319e3acee843bbd7a0ce838727249881878983
leebs <leebs@59.5.237.13>
2024-05-15 20:12:34 +0900

    코드 수정

--- ch13/src/sec04/ex05/DefaultTest.java
+++ ch13/src/sec04/ex05/DefaultTest.java
@@ -4,6 +4,7 @@ public class DefaultTest {
 
 	public static void main(String[] args) {
 		InterfaceA interfaceA = new ImplA();
+		interfaceA.method1();
 		interfaceA.defaultMethod();
 		InterfaceA.staticMethod();
 	}
