1336d88211e173222ca8c4a3026a8315c9c60492
leebs <leebs@59.5.237.13>
2024-05-16 12:02:01 +0900

    주석추가

--- rentCarApp/src/com/oracle/rent/ch13/main/RentCarApp.java
+++ rentCarApp/src/com/oracle/rent/ch13/main/RentCarApp.java
@@ -20,21 +20,21 @@ public class RentCarApp {
 		System.out.println("----------------------------------------------------------------");
 		MemberVO memberVO = new MemberVO("lee", "1234", "이순신", "서울시 도봉구", "010-1111-2222");
 
-		Member member = new MemberImpl();
+		Member member = new MemberImpl();  //인터페이스 참조 변수에 업캐스팅
 		member.regMember(memberVO); // 회원 가입하기
 		memData = member.viewMember(memberVO); // 회원 정보 조회하기
 		((AbstractBase)member).displayData(memData);
 		System.out.println("----------------------------------------------------------------");
 
 		CarVO carVO = new CarVO("11가1111", "소나타", "검정", 2000, "현대");
-		Car car = new CarImpl();
+		Car car = new CarImpl(); //인터페이스 참조 변수에 업캐스팅
 		car.regCarInfo(carVO); // 새 차 정보 등록
 		carData = car.checkCarInfo(carVO); // 차 정보 조회
 		((AbstractBase)car).displayData(carData);
 		System.out.println("----------------------------------------------------------------");
 
 		ResVO resVO = new ResVO("20220708-0001", "11가1111", "2022-04-30", "2022-05-01", "2022-05-08");
-		Reserve reserve = new ReserveImpl();
+		Reserve reserve = new ReserveImpl();  //인터페이스 참조 변수에 업캐스팅
 		reserve.resCar(resVO); // 차 예약하기
 		resData = reserve.checkResInfo(resVO);
 		((AbstractBase)reserve).displayData(resData);
