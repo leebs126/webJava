6399cf7ccbcbe2c37a77f380c5c865e6110d7449
leebs <leebs@59.5.237.13>
2024-05-17 18:25:19 +0900

    공백 제거

--- ch14/src/sec01/ex01/InnerTest.java
+++ ch14/src/sec01/ex01/InnerTest.java
@@ -3,8 +3,8 @@ package sec01.ex01;
 public class InnerTest {
 	public static void main(String[] args) {
 		Outter outter = new Outter("홍길동" , 20 );
-		Outter.Inner inner = outter.new Inner( "서울시 종로구" );
-		System.out.println( "고객정보 >> " + inner.getUserInfo( ) );
+		Outter.Inner inner = outter.new Inner("서울시 종로구");
+		System.out.println( "고객정보 >> " + inner.getUserInfo());
 	}
 
 }
