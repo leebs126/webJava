ae2dad848cafe9a5887004ef2f808cdd2d67191d
leebs <leebs@220.72.44.129>
2024-05-26 20:02:48 +0900

    주석 수정

--- ch15/src/sec01/ex01/Student.java
+++ ch15/src/sec01/ex01/Student.java
@@ -15,7 +15,7 @@ public class Student extends Object {
 		this.grade = grade;
 	}
 
-	// 학생 신상 정보 출력 메소드
+	// 학생 신상 정보 출력 메서드
 	public String getStudInfo() {
 		System.out.println("Student 클래스의 getStudInfo() 메소드입니다.");
 		return "이름: " + name + " , 학년: " + grade;
