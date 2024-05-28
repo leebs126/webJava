4e823e2dccbbe15031f95c3e8de0efce1a26e3ff
leebs <leebs@220.72.44.129>
2024-05-28 16:08:52 +0900

    코드 수정

--- ch15/src/sec01/ex06/ToStringTest.java
+++ ch15/src/sec01/ex06/ToStringTest.java
@@ -5,7 +5,7 @@ public class ToStringTest {
 	public static void main(String[] args) {
 		Student s = new Student("이순신", 2);
 		
-		System.out.println(s.toString( ));
+		System.out.println(s.toString());
 		System.out.println(s);
 		
 
