b954c2d43661983102c032d2347fbbb91b2d00d7
leebs <leebs@220.72.44.129>
2024-05-28 13:02:07 +0900

    코드 정리

--- ch15/src/sec01/ex04/StudentEqualsTest.java
+++ ch15/src/sec01/ex04/StudentEqualsTest.java
@@ -4,8 +4,8 @@ public class StudentEqualsTest {
 
 	public static void main(String[] args) {
 		Student s1 = new Student("이순신", 2);
-		Student s2 = new Student("이순신",2);
-		Student s3 = new Student("홍길동",3);
+		Student s2 = new Student("이순신", 2);
+		Student s3 = new Student("홍길동", 3);
 		
 		System.out.println(s1.equals(s2));
 		System.out.println(s1.equals(s3));
