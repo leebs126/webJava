0a1042e5803c275e2d2a22f696585fa718527292
leebs <leebs@220.72.44.129>
2024-05-29 21:38:28 +0900

    코드 수정

--- ch15/src/sec02/ex02/RandomTest.java
+++ ch15/src/sec02/ex02/RandomTest.java
@@ -7,11 +7,11 @@ public class RandomTest {
 
         Random random1 = new Random();
 
-        System.out.println( random1.nextInt() ); // -2,147,483,648 ~ 2,147,483,647 사이의 값
-        System.out.println( random1.nextInt(5) );  // 0 ~ 5 사이의 값
-        System.out.println( random1.nextInt(5) + 1 );  //1 ~ 6 사이의 값
+        System.out.println( random1.nextInt()); // -2,147,483,648 ~ 2,147,483,647 사이의 값
+        System.out.println( random1.nextInt(5));  // 0 ~ 5 사이의 값
+        System.out.println( random1.nextInt(5) + 1);  //1 ~ 6 사이의 값
         System.out.println(random1.nextDouble());  //0.0 ~ 1.0사이의 실수
-        System.out.println( random1.nextBoolean() );  //true or false
+        System.out.println( random1.nextBoolean());  //true or false
 
         System.out.println();
         Random random2 = new Random(10);
