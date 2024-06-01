be1ab34c58bc04d30206561a940dbaeac539e4b7
leebs <leebs@220.72.44.129>
2024-06-01 21:12:21 +0900

    코드 수정

--- ch15/src/sec03/ex03/GetBytesTest.java
+++ ch15/src/sec03/ex03/GetBytesTest.java
@@ -5,19 +5,14 @@ import java.io.UnsupportedEncodingException;
 public class GetBytesTest {
 
 	public static void main(String[] args) {
-		String name = "이순신";
+		String name = "자바 프로그래밍";
 		byte[] encodeBytes = null;
 		
 		try {
 			encodeBytes = name.getBytes("UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			String decodeName1 = new String(encodeBytes, "UTF-8");
 			System.out.println(decodeName1);
-			
+		
 			byte[] b ="hello".getBytes();
 			System.out.println(b.length);
 			String decodeName2 = new String(b, 2, 3,"UTF-8");
