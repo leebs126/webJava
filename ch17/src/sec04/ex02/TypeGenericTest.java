a19d2ff47e8bde89fed4102a7576c60ffaeb2fa8
leebs <leebs@59.5.131.227>
2024-07-02 20:52:16 +0900

    주석 수정

--- ch17/src/sec04/ex02/TypeGenericTest.java
+++ ch17/src/sec04/ex02/TypeGenericTest.java
@@ -6,7 +6,7 @@ public class TypeGenericTest {
 		GOrigin<String> origin1 = new GOrigin<String>();
 		String str = "안녕하세요";
 		origin1.setData(str);
-//		origin1.setData(new Integer(300)); String 타입외에 다른 타입을 메소드로 전달 시 에러를 발생시킵니다.
+//		origin1.setData(new Integer(300)); String 타입외에 다른 타입을 메서드로 전달 시 에러를 발생시킵니다.
 		String result1 = origin1.getData();
 //		String result1 = (String)origin1.getData();
 		System.out.println(result1);
@@ -14,7 +14,7 @@ public class TypeGenericTest {
 		GOrigin<Integer> origin2 = new GOrigin<Integer>();
 		int num = 100;
 		origin2.setData(new Integer(num));
-//		origin2.setData(str); //Integer 객체외 다른 타입을 메소드로 전달 시 에러를 발생시킵니다.
+//		origin2.setData(str); //Integer 객체외 다른 타입을 메서드로 전달 시 에러를 발생시킵니다.
 		Integer result2 = origin2.getData();
 //		Integer result2 = (Integer)origin2.getData();
 		System.out.println(result2);
