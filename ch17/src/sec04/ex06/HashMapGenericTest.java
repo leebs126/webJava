8939146ef1d38277d3895c32cdf60d359b727fe1
leebs <leebs@59.5.131.227>
2024-07-04 16:11:56 +0900

    공백제거

--- ch17/src/sec04/ex06/HashMapGenericTest.java
+++ ch17/src/sec04/ex06/HashMapGenericTest.java
@@ -19,7 +19,7 @@ public class HashMapGenericTest {
 //		String addr = (Integer)hMap1.get("홍길동");  //컴파일 에러가 발생합니다.
 		System.out.println(addr);
 
-		Map<String, Student> hMap2 = new HashMap<String, Student >();
+		Map<String, Student> hMap2 = new HashMap<String, Student>();
 		hMap2.put("서울시", new Student("이순신", 3));
 		hMap2.put("부산시", new Student("홍길동", 2));
 		hMap2.put("대구시", new Student("유현진", 1));
