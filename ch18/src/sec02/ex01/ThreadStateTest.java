7842b70c1f7ffbcbc497da88d89e3a379a17b277
leebs <leebs@118.32.103.226>
2024-07-22 21:22:20 +0900

    주석 추가

--- ch18/src/sec02/ex01/ThreadStateTest.java
+++ ch18/src/sec02/ex01/ThreadStateTest.java
@@ -18,21 +18,22 @@ public class ThreadStateTest {
 		};
 		
 		state = thread.getState();
-		System.out.println("스레드 상태: " + state);
+		System.out.println("스레드 상태: " + state);  //NEW
 		
 		thread.start();
-		state = thread.getState();
-		System.out.println("스레드 상태: " + state);
+		state = thread.getState(); 
+		System.out.println("스레드 상태: " + state);  //RUNNABLE
 		
 		thread.interrupt();  //스레드를 강제 종료 시킵니다.
 		
+		//interrute() 메서드 호출 시 일단 대기 상태로 전환해서 종료합니다. 
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 		}
 		
 		state = thread.getState();
-		System.out.println("스레드 상태: " + state);
+		System.out.println("스레드 상태: " + state);  // TERMINATED
 		
 		System.out.println("메인 스레드 종료");
 
