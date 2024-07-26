4fc829f41c96091fb3294604c7726f3149bb1602
leebs <leebs@118.32.103.226>
2024-07-26 18:18:12 +0900

    주석 수정

--- ch18/src/sec02/ex03/Horse.java
+++ ch18/src/sec02/ex03/Horse.java
@@ -11,14 +11,13 @@ public class Horse extends Thread {
 	public void run() {
 		while (true) {
 			if (yieldFlag) {
-				
-				System.out.println(horseNum +"번 경마가 지나갑니다.");
-				try { Thread.sleep(1000); } catch (InterruptedException e) {}
-
-				Thread.yield();  //실행 중인 스레드는 실행 대기로 전환됩니다.
+				Thread.yield();  //실행 중인 스레드는 실행 준비 상태로 전환됩니다.
 				yieldFlag =false;
 			} else {
 				yieldFlag =true;
+				System.out.println(horseNum +"번 경마가 지나갑니다.");
+				try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
 				for (int i = 0; i < 1000000000; i++) {}
 				
 			}
