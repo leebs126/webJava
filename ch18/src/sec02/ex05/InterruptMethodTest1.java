606e3cbb26d8b08131e93ae6f845d4339de44cf9
leebs <leebs@118.32.103.226>
2024-07-27 14:37:38 +0900

    주석 추가

--- ch18/src/sec02/ex05/InterruptMethodTest1.java
+++ ch18/src/sec02/ex05/InterruptMethodTest1.java
@@ -12,7 +12,7 @@ public class InterruptMethodTest1  {
 		videoThread.interrupt(); //메서드 호출 후 실제 예외 발생까지 시간이 걸립니다.
 		System.out.println("videoThread 상태: " + videoThread.getState());
 		
-		Thread.sleep(100);
+		Thread.sleep(100); //현재 실행 중인 스레드를 일지 정지 상태로 만듭니다.
 		System.out.println("videoThread 상태: " + videoThread.getState());
 
 		System.out.println("메인 스레드 종료");
