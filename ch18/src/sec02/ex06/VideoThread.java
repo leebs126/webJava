9f0a4fe9a4663b85c9c2e7d2c7c6be968e549ba9
leebs <leebs@220.72.44.129>
2024-07-29 12:43:03 +0900

    코드 수정

--- ch18/src/sec02/ex06/VideoThread.java
+++ ch18/src/sec02/ex06/VideoThread.java
@@ -8,6 +8,7 @@ public class VideoThread extends Thread {
 	public void run() {
 		while(true) {
 			System.out.println("동영상을 재생합니다.");
+			
 			if(Thread.interrupted()) {
 				System.out.println("동영상 재생 강제 종료");
 				break;
