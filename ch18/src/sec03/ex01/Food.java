c324794ee1d9e61b4051e86ad561d23634e77bff
leebs <leebs@220.72.44.129>
2024-07-31 18:55:14 +0900

    코드 수정

--- ch18/src/sec03/ex01/Food.java
+++ ch18/src/sec03/ex01/Food.java
@@ -13,7 +13,7 @@ public class Food {
 			System.out.println("웨이터가 손님에게 주문을 받습니다.");
 		}
 		
-		int menuNum = (int)(Math.random() * 3) + 0;
+		int menuNum = (int)(Math.random() * 3);
 		foodName = menu[menuNum]; //메뉴에서 주문한 음식 이름을 저장합니다.
 		
 		System.out.println("웨이터가 " + foodName+" 주문을 주방에 전달합니다.");
