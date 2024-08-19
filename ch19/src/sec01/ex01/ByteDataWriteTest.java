893285a6d992e489473843eae009f545c95a06bb
leebs <leebs@leebscom>
2024-08-19 13:30:09 +0900

    코드 수정

--- ch19/src/sec01/ex01/ByteDataWriteTest.java
+++ ch19/src/sec01/ex01/ByteDataWriteTest.java
@@ -8,8 +8,8 @@ public class ByteDataWriteTest {
 
 	public static void main(String[] args) {
 		try {
-//			FileOutputStream fos = new FileOutputStream("C:\\test\\test1.txt");
-			FileOutputStream fos = new FileOutputStream("C:\\test\\test1.txt", true);
+			FileOutputStream fos = new FileOutputStream("C:\\test\\test1.txt");
+//			FileOutputStream fos = new FileOutputStream("C:\\test\\test1.txt", true);
 			
 			Scanner sc = new Scanner(System.in);
 			System.out.println("단어를 입력해 주세요.");
@@ -22,11 +22,13 @@ public class ByteDataWriteTest {
 			
 			fos.flush();
 			fos.close();
+			sc.close();
 			
 			System.out.println("파일에 출력했습니다.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
