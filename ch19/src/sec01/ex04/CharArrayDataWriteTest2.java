a80296eb386ef3122e2a2b224584368b9d302669
leebs <leebs@leebscom>
2024-08-23 20:19:21 +0900

    주석 추가

--- ch19/src/sec01/ex04/CharArrayDataWriteTest2.java
+++ ch19/src/sec01/ex04/CharArrayDataWriteTest2.java
@@ -11,7 +11,7 @@ public class CharArrayDataWriteTest2 {
 			
 			char[] cArray = {'자' , '바' ,' ', '프', '로', '그', '래', '밍', '입', '니', '다', '.'}; 
 			
-			writer.write(cArray, 1, 7);
+			writer.write(cArray, 1, 7);  //두번째 문자 포함해서 7자를 출력합니다.
 			writer.flush();
 			writer.close();
 			
