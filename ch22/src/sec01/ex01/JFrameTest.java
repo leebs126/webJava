5501b572b93d1283e40d5462684cacf1ef577eac
PC <PC@220.72.44.10>
2024-10-20 13:20:41 +0900

    코드수정

--- ch22/src/sec01/ex01/JFrameTest.java
+++ ch22/src/sec01/ex01/JFrameTest.java
@@ -6,25 +6,26 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class JFrameTest {
-	JFrame jFrame = new JFrame("JFrame 테스트");
+	JFrame jFrame;
 	JButton btn1, btn2, btn3, btn4;
+	JTextField tf1;
 	
 	public void createFrame() {
-		JButton btn1 = new JButton("button1");
-		JButton btn2 = new JButton("button2");
-		JButton btn3 = new JButton("button3");
-		JButton btn4 = new JButton("button4");
-		JTextField tf1 = new JTextField("내용을 입력하세요.");
+		jFrame = new JFrame("JFrame 테스트");
+		btn1 = new JButton("button1");
+		btn2 = new JButton("button2");
+		btn3 = new JButton("button3");
+		btn4 = new JButton("button4");
+		tf1 = new JTextField("내용을 입력하세요.");
 		
-		
-		//프레임에 컴포넌트 추가
+		//Frame에 컴포넌트 추가
 		jFrame.add(tf1, BorderLayout.NORTH);
 		jFrame.add(btn1, BorderLayout.SOUTH);
 		jFrame.add(btn2, BorderLayout.WEST);
 		jFrame.add(btn3, BorderLayout.EAST);
 		jFrame.add(btn4, BorderLayout.CENTER);
 		
-		jFrame.setPreferredSize(new Dimension(500,500)); // 프레임의 너비와 높이 지정		
+		jFrame.setPreferredSize(new Dimension(500,500)); // Frame의 너비와 높이 지정		
 		jFrame.pack();		
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // frame 종료		
 		jFrame.setVisible(true);
