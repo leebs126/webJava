30d348001f09ced6e2592956dee83d00184501da
PC <PC@121.129.214.113>
2024-11-16 12:38:35 +0900

    sql문 수정

--- rentCarApp/sql/렌터카 프로그램 테이블.sql
+++ rentCarApp/sql/렌터카 프로그램 테이블.sql
@@ -1,5 +1,5 @@
 --회원 테이블
-create table t_member2(
+create table t_Member(
 memId VARCHAR2(20) PRIMARY KEY, --아이디
 memPassword VARCHAR2(20), --비밀번호
 memName VARCHAR2(30), --이름
@@ -9,7 +9,7 @@ memPhoneNum VARCHAR2(50) --전화번호
 
 
 --렌터카 테이블
-CREATE TABLE t_Car2 (
+CREATE TABLE t_Car(
 carNumber VARCHAR2(30) PRIMARY KEY, --차 번호
 carName VARCHAR2(50), --차 이름
 carColor VARCHAR2(30), --차 색상
@@ -18,13 +18,13 @@ carMaker VARCHAR2(50) --제조사
 );
 
 --예약 테이블
-CREATE TABLE t_Res2 (
+CREATE TABLE t_Res(
 resNumber VARCHAR2(30) PRIMARY KEY, --예약 번호
 resDate DATE, --예약일자
 useBeginDate DATE, --이용 시작일자
 returnDate DATE, --반납일자
 resCarNumber VARCHAR2(30), --예약 차 번호
 resUserId VARCHAR2(20), --예약자 아이디
-CONSTRAINT FK_RES_USER_ID FOREIGN KEY(resUserId) REFERENCES t_Member2 (memId),
-CONSTRAINT FK_RES_CAR_NUMBER FOREIGN KEY(resCarNumber) REFERENCES t_Car2 (CarNumber)
+CONSTRAINT FK_RES_USER_ID FOREIGN KEY(resUserId) REFERENCES t_Member(memId),
+CONSTRAINT FK_RES_CAR_NUMBER FOREIGN KEY(resCarNumber) REFERENCES t_Car(CarNumber)
 );
