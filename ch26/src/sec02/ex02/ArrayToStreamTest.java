5409e11677442c4ac84965b48d35918d1c152e31
PC <PC@220.72.89.25>
2024-12-06 18:49:21 +0900

    코드 정리

--- ch26/src/sec02/ex02/ArrayToStreamTest.java
+++ ch26/src/sec02/ex02/ArrayToStreamTest.java
@@ -8,12 +8,11 @@ import sec02.ex01.Student;
 
 public class ArrayToStreamTest {
 	public static void main(String[] args) {
-		Student[] stdArr = {
-					new Student("홍길동", 2),
-					new Student("이순신", 3),
-					new Student("박찬호", 1),
-					new Student("손흥민", 2)
-		};
+		Student[] stdArr = {new Student("홍길동", 2),
+							new Student("이순신", 3),
+							new Student("박찬호", 1),
+							new Student("손흥민", 2)};
+		
 		Stream<Student> stdStream = Arrays.stream(stdArr);
 		stdStream.forEach(s->System.out.println(s.getName()));
 		System.out.println();
