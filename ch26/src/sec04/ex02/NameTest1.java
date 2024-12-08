da0dfea003e0634682a5744e7fa8a12d88f1114d
PC <PC@220.72.89.58>
2024-12-08 18:37:26 +0900

    예제 파일 추가

--- ch26/src/sec04/ex02/NameTest1.java
+++ ch26/src/sec04/ex02/NameTest1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//flatMap() 메서드 이용하지 않고 "홍길동" 이름만 제외해서 출력하기
 public class NameTest1 {
 
 	public static void main(String[] args) {
@@ -11,20 +12,21 @@ public class NameTest1 {
 			                               {"박지성", "차범근"}, 
 			                               {"손흥민", "임꺽정"}};
 			                               
-        List<String> nameList = new ArrayList<>();
+      List<String> nameList = new ArrayList<>();
 
-        //2차원 배열에서 중첩 for문으로 접근하기
-        for (String[] arrName : nested) {
-            for (String name : arrName) {
-                if(!name.equals("홍길동")) nameList.add(name);
-            }
-        }
+      //2차원 배열에서 중첩 for문으로 접근하기
+      for (String[] arrName : nested) {
+          for (String name : arrName) {
+              if(!name.equals("홍길동")) nameList.add(name);
+          }
+      }
 
-        String[] exclude = nameList.toArray(new String[nameList.size()]);
-        System.out.println(Arrays.toString(exclude));		
+      String[] exclude = nameList.toArray(new String[nameList.size()]);
+      System.out.println(Arrays.toString(exclude));		
 
 	}
 }
+
 
 
 
--- ch26/src/sec04/ex02/NameTest2.java
+++ ch26/src/sec04/ex02/NameTest2.java
@@ -3,23 +3,22 @@ package sec04.ex02;
 import java.util.Arrays;
 import java.util.List;
 
+//flatMap() 메서드 이용해서 "홍길동" 이름만 제외해서 출력하기
 public class NameTest2 {
 
 	public static void main(String[] args) {
 		String[][] nested = new String[][]{{"홍길동", "이순신"}, 
 			                               {"박지성", "차범근"}, 
 			                               {"손흥민", "임꺽정"}};
-			                               
-        List<String[]> nameList = Arrays.stream(nested)
-		                .filter(arrName -> { 			// nested가 2차원 배열이기 때문에 실행 시 전달되는 매개값은 1차원 문자열 배열 
-		                    for (String name : arrName) { //arrName이 1차원 배열이기 때문에 다시 순회하기 위해 for문 사용
-		                        if (name.equals("홍길동")) return false;
-		                    }
-		                    return true;
-		                })
-		                .toList(); //일차원 배열 ArrayList로 리턴
-        nameList.forEach(name-> System.out.println(Arrays.toString(name)));		
+
+	        List<String> nameList = Arrays.stream(nested)  //일차원 배열 Stream 객체 리턴
+	                .flatMap(arrName -> Arrays.stream(arrName)) //매개변수로 전달된 일차원 배열의 요소들을 문자열로 분해해서 Stream으로 리턴
+	                .filter(name -> !name.equals("홍길동"))
+	                .toList();
+
+	        nameList.forEach(System.out::println);		
 	}
 }
+
 
 
