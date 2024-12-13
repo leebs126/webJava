bee35ee2e86dca461a3c18a6188fe6683e3ced4b
PC <PC@119.192.78.102>
2024-12-13 19:23:11 +0900

    주석 수정

--- ch26/src/sec04/ex05/PeekTest.java
+++ ch26/src/sec04/ex05/PeekTest.java
@@ -8,7 +8,7 @@ public class PeekTest {
 	public static void main(String[] args) {
 		List<String> fruitList = Arrays.asList("apple","banana","apple","mango","strawberry","banana", "mango");
 		
-		//최종 처리 메소드가 호출되어야 중간 처리 메소드가 실행됩니다.
+		//최종 처리 메소드가 호출되어야 peek() 메소드가 실행됩니다.
 //		fruitList.stream()
 //		.distinct()
 //		.filter(s -> s.length() < 8)
@@ -16,8 +16,8 @@ public class PeekTest {
 
 		
 		fruitList.stream()
-			.distinct()  					//"apple", "banana", "mango", "strawberry" 
-			.filter(s -> s.length() < 8)    // "apple", "banana", "mango"
+			.distinct()  					//"apple", "banana", "mango", "strawberry"
+			.filter(s -> s.length() < 8)    //"apple", "banana", "mango"
 			.peek(a->System.out.println(a))
 			.filter(s -> s.startsWith("a"))
 			.peek(a->System.out.println(a))
