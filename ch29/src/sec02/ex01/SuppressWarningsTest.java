ce26f83215367af7045f0234a3ccdccfa832c94b
PC <PC@220.72.43.30>
2024-12-28 16:05:46 +0900

    코드 정리

--- ch29/src/sec02/ex01/SuppressWarningsTest.java
+++ ch29/src/sec02/ex01/SuppressWarningsTest.java
@@ -7,6 +7,6 @@ public class SuppressWarningsTest {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) {
 		List studentList = new ArrayList();  //"unchecked로 지정하므로 경고가 발생하지 않습니다.
-		studentList.add(new  Object());   //rawtypes로 지정하므로 경고가 발생하지 않습니다.
+		studentList.add(new  Object());      //rawtypes로 지정하므로 경고가 발생하지 않습니다.
 	}
 }
