55e47566e7599d6195be9509a4b1681ce6cfd4ab
PC <PC@220.72.44.145>
2025-01-08 19:10:53 +0900

    파일 이름 수정

--- ch30/src/sec06/ex01/LocalDateAndTimeTest.java
+++ ch30/src/sec06/ex01/LocalDateAndTimeTest.java
@@ -6,25 +6,28 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
-public class LocalDataAndTimeTest {
+public class LocalDateAndTimeTest {
 	public static void main(String[] args) {
-		LocalDate currDate = LocalDate.now();
+		LocalDate currDate = LocalDate.now(); //현재 로컬 날짜 
 		System.out.println(currDate);
 		
-		LocalDate targetDate = LocalDate.of(2023, 7, 22);
-		System.out.println(targetDate);
-		System.out.println();
+		LocalDate targetDate = LocalDate.of(2024, 7, 22);
+		System.out.println(targetDate);     
+		System.out.println(); 
 		
 		//LocalTime 클래스
-		LocalTime currTime = LocalTime.now();
+		LocalTime currTime = LocalTime.now();   //현재 로컬의 시각
 		System.out.println(currTime);
 		System.out.println();
 		
-		LocalDateTime currDateTime = LocalDateTime.now();
+		LocalDateTime currDateTime = LocalDateTime.now(); //현재 로컬 날짜와 시각 정보  
 		System.out.println(currDateTime);
 		System.out.println();
 		
-		Instant instant1 = Instant.now();
+//		System.currentTimeMillis();
+//		System.nanoTime();
+		
+		Instant instant1 = Instant.now();   //타임스탬프 
 		for(int i = 0 ; i< 10000000;i++) {}
 		Instant instant2 = Instant.now();
 		
