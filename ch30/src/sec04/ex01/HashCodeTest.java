f72492688ccf79df9f52ff3756dd7598c74da7bc
PC <PC@220.72.44.145>
2025-01-08 13:18:18 +0900

    static 삭제

--- ch30/src/sec04/ex01/HashCodeTest.java
+++ ch30/src/sec04/ex01/HashCodeTest.java
@@ -6,28 +6,30 @@ public class HashCodeTest {
 	public static void main(String[] args) {
 		Student s1 = new Student("홍길동", 1, 90);
 		Student s2 = new Student("홍길동", 1, 90);
+		
 		System.out.println(s1.hashCode());
 		System.out.println(Objects.hashCode(s2));
 	}
+}
 
-	static class Student{
-		String name;
-		int grade;
-		int score;
-		public Student(String name, int grade, int score) {
-			this.name = name;
-			this.grade = grade;
-			this.score = score;
-		}
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(name, grade, score);
-		}
-		
-		
+
+class Student {
+	String name;
+	int grade;
+	int score;
+	
+	public Student(String name, int grade, int score) {
+		this.name = name;
+		this.grade = grade;
+		this.score = score;
 	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, grade, score);
+	}
 }
+	
+
 
 
