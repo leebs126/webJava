13f345d45e1ba972f6fd95221af0aa97c91c4bde
PC <PC@220.72.44.145>
2025-01-12 22:26:34 +0900

    주석 추가

--- ch30/src/sec08/ex04/CharsetConversionTest.java
+++ ch30/src/sec08/ex04/CharsetConversionTest.java
@@ -5,20 +5,20 @@ import java.nio.charset.Charset;
 
 public class CharsetConversionTest {
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String originalStr = "안녕";   //UTF-8
+		String originalStr = "안녕";   // UTF-8
 		System.out.println("원본 문자열: " + originalStr);
 		System.out.println();
 		
-		byte[] euckrStringBuffer = originalStr.getBytes(Charset.forName("EUC-KR")); // String 을 EUC-KR 로 인코딩
+		byte[] euckrStringBuffer = originalStr.getBytes(Charset.forName("EUC-KR")); // String 을 EUC-KR 바이트 데이터로 변환 
 		System.out.println("EUC-KR 문자열 길이: " + euckrStringBuffer.length);
-		String decodedFromEucKr = new String(euckrStringBuffer, "EUC-KR");
-		System.out.println("EUC-KR로 변환된 문자열 : " + decodedFromEucKr);// String 을 UTF-8 로 인코딩.
+		String decodedFromEucKr = new String(euckrStringBuffer, "EUC-KR");   // 바이트 데이터를 EUC-KR 문자열로 변환
+		System.out.println("EUC-KR로 변환된 문자열 : " + decodedFromEucKr);   
 
-		byte[] utf8StringBuffer = decodedFromEucKr.getBytes("UTF-8");
+		byte[] utf8StringBuffer = decodedFromEucKr.getBytes("UTF-8");  // String 을 UTF-8 바이트 데이터로 변환
 		System.out.println();
 		
-		System.out.println("UTF-8 문자열 길이: " + utf8StringBuffer.length);
-		String decodedFromUtf8 = new String(utf8StringBuffer, "UTF-8");
+		System.out.println("UTF-8 문자열 길이: " + utf8StringBuffer.length); 
+		String decodedFromUtf8 = new String(utf8StringBuffer, "UTF-8");  // 바이트 데이터를 UTF-8 문자열로 변환
 		System.out.println("UTF-8로 변환된 문자열: " + decodedFromUtf8);
 	}
 
