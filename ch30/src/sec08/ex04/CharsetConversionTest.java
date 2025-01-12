eb0d4c32dae37bddbae50c14ed851b473bee9271
PC <PC@220.72.44.145>
2025-01-12 22:02:42 +0900

    코드 정리

--- ch30/src/sec08/ex04/CharsetConversionTest.java
+++ ch30/src/sec08/ex04/CharsetConversionTest.java
@@ -5,12 +5,11 @@ import java.nio.charset.Charset;
 
 public class CharsetConversionTest {
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String originalStr = "안녕";
+		String originalStr = "안녕";   //UTF-8
 		System.out.println("원본 문자열: " + originalStr);
+		System.out.println();
 		
 		byte[] euckrStringBuffer = originalStr.getBytes(Charset.forName("EUC-KR")); // String 을 EUC-KR 로 인코딩
-		System.out.println();
-
 		System.out.println("EUC-KR 문자열 길이: " + euckrStringBuffer.length);
 		String decodedFromEucKr = new String(euckrStringBuffer, "EUC-KR");
 		System.out.println("EUC-KR로 변환된 문자열 : " + decodedFromEucKr);// String 을 UTF-8 로 인코딩.
