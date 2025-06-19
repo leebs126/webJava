98ce31bed4b8ee24f2f8f9da8cceb57f091878df
leebs <leebs@121.129.214.118>
2025-06-19 18:16:56 +0900

    DataUtil 클래스 생성자 호출 금지

--- rentCarApp/src/com/oracle/rent/ch13/common/DataUtil.java
+++ rentCarApp/src/com/oracle/rent/ch13/common/DataUtil.java
@@ -1,6 +1,11 @@
 package com.oracle.rent.ch13.common;
 
 public class DataUtil {
+	//인스턴스 생성을 금지합니다.
+	private  DataUtil() {
+		
+	}
+		
 	public static void encodeData(String data) {
 		System.out.println("\n" +data + "를 암호화합니다.");
 	}
--- rentCarApp/src/com/oracle/rent/ch19/common/DataUtil.java
+++ rentCarApp/src/com/oracle/rent/ch19/common/DataUtil.java
@@ -1,6 +1,12 @@
 package com.oracle.rent.ch19.common;
 
 public class DataUtil {
+	//인스턴스 생성을 금지합니다.
+	private  DataUtil() {
+		
+	}
+		
+
 	public static void encodeData(String data) {
 		System.out.println("\n" +data + "를 암호화합니다.");
 	}
